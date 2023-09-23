832e07a77370d30e75c9589f0a19790b073f6a7e
Reggeditt <tkodjosarso@gmail.com>
2023-09-23 18:27:34 +0000

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :categories
   has_many :cashflows
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
